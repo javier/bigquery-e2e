61767d0f07ab40a45d33238ea6a69c3f2702c4d2
Siddartha Naidu <snaidu@localhost>
2014-02-17 19:50:02 -0800

    Delete generated files from source control.

--- sensors/client/gen/com/googlecode/bigquery_e2e/sensors/client/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.googlecode.bigquery_e2e.sensors.client;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
