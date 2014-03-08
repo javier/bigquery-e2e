8b0aab7777db989d9e3b5eed2488b5616467686b
Siddartha Naidu <snaidu@localhost>
2014-03-08 07:51:50 -0800

    Fix up chapter 5 to use the new auth module.

--- samples/ch05/tour.py
+++ samples/ch05/tour.py
@@ -6,7 +6,6 @@ import json
 import time
 
 # Google APIs imports
-from apiclient.discovery import build
 from apiclient.errors import HttpError
 from apiclient.http import MediaIoBaseUpload
 
@@ -14,7 +13,7 @@ from apiclient.http import MediaIoBaseUpload
 import auth
 
 ## Runs through each BigQuery API request.
-def run_tour(http, service, project_id):
+def run_tour(service, project_id):
   print 'Running BigQuery API tour'
 
   projects = service.projects()
@@ -39,43 +38,43 @@ def run_tour(http, service, project_id):
 	     'projectId': project_id}
 
   # First, find the project and print out the friendly name.
-  for project in projects.list().execute(http)['projects']:
+  for project in projects.list().execute()['projects']:
      if (project['id'] == project_id):
        print "Found %s: %s" % (project_id, project['friendlyName'])
 
   # Now create a dataset
   dataset = {'datasetReference': dataset_ref}
-  dataset = datasets.insert(body=dataset, **project_ref).execute(http)
+  dataset = datasets.insert(body=dataset, **project_ref).execute()
 
   # Patch the dataset to set a friendly name.
   update = {'friendlyName': 'Tour dataset'}
-  dataset = datasets.patch(body=update, **dataset_ref).execute(http)
+  dataset = datasets.patch(body=update, **dataset_ref).execute()
 
   # Print out the dataset for posterity
   print "%s" % (dataset,)
 
   # Find our dataset in the datasets list:
-  dataset_list = datasets.list(**project_ref).execute(http)
+  dataset_list = datasets.list(**project_ref).execute()
   for current in dataset_list['datasets']:
     if current['id'] == dataset['id']: print "found %s" % (dataset['id'])
 
   ### Now onto tables...
   table = {'tableReference': table_ref}
-  table = tables.insert(body=table, **dataset_ref).execute(http)
+  table = tables.insert(body=table, **dataset_ref).execute()
 
   # Update the table to add a schema:
   table['schema'] = {'fields': [{'name': 'a', 'type': 'string'}]}
-  table = tables.update(body=table, **table_ref).execute(http)
+  table = tables.update(body=table, **table_ref).execute()
 
   # Patch the table to add a friendly name
   patch = {'friendlyName': "Friendly table"}
-  table = tables.patch(body=patch, **table_ref).execute(http)
+  table = tables.patch(body=patch, **table_ref).execute()
 
   # Print table for posterity:
   print table
 
   # Find our table in the tables list:
-  table_list = tables.list(**dataset_ref).execute(http)
+  table_list = tables.list(**dataset_ref).execute()
   for current in table_list['tables']:
     if current['id'] == table['id']: print "found %s" % (table['id'])
 
@@ -87,41 +86,41 @@ def run_tour(http, service, project_id):
   job = {'jobReference': job_ref, 'configuration': config}
 
   media = MediaIoBaseUpload(io.BytesIO(load_text), mimetype='application/octet-stream')
-  job = jobs.insert(body=job, media_body=media, **project_ref).execute(http)
+  job = jobs.insert(body=job, media_body=media, **project_ref).execute()
 
   # List our running or pending jobs:
   job_list = jobs.list(
       stateFilter=['pending', 'running'], 
-      **project_ref).execute(http)
+      **project_ref).execute()
   print job_list
 
   while job['status']['state'] <> 'DONE':
-    job = jobs.get(**job_ref).execute(http)
+    job = jobs.get(**job_ref).execute()
 
   # Now run a query against that table.
   query = 'select count(*) from [%s]' % (table['id'])
   query_request = {'query': query, 'timeoutMs': 0, 'maxResults': 1}
-  results = jobs.query(body=query_request, **project_ref).execute(http)
+  results = jobs.query(body=query_request, **project_ref).execute()
   while not results['jobComplete']:
     get_results_request = results['jobReference'].copy()
     get_results_request['timeoutMs'] = 10000
     get_results_request['maxResults'] = 10
     results = jobs.getQueryResults(
-	**get_results_request).execute(http)
+	**get_results_request).execute()
   print results    
 
   # Now let's read the data from our table.
-  data = tabledata.list(**table_ref).execute(http)
-  table = tables.get(**table_ref).execute(http)
+  data = tabledata.list(**table_ref).execute()
+  table = tables.get(**table_ref).execute()
   print 'Table %s\nData:%s' % (data, table)
 
   # Now we should clean up our toys.
-  tables.delete(**table_ref).execute(http)
-  datasets.delete(**dataset_ref).execute(http)
+  tables.delete(**table_ref).execute()
+  datasets.delete(**dataset_ref).execute()
 
   # Now try reading the datast after deleting it:
   try:
-    datasets.get(**dataset_ref).execute(http)
+    datasets.get(**dataset_ref).execute()
     print "That's funny, we should never get here!"
   except HttpError as err:
     print err
@@ -129,11 +128,9 @@ def run_tour(http, service, project_id):
   # Done!
 
 def main():
-  creds = auth.get_creds()
-  http = auth.authorize(creds)
-  service = build('bigquery', 'v2')
+  service = auth.build_bq_client() 
   project_id = 'bigquery-e2e'
-  run_tour(http, service, project_id)
+  run_tour(service, project_id)
 
 if __name__ == "__main__":
     main()
